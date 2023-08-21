730cdef15577d033f051d27101d4fb33b7cc5db0
Sayali Nikam <137673498+SayaliGN@users.noreply.github.com>
2023-08-21 21:02:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let firstName = document.getElementById("fname").value;
+	let firstName = document.getElementById("lname").value;
+	alert(`${firstName} ${lastName}`);
 }
